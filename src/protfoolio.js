118985cb1cf63e6210706f10a0a20599b148b8a3
Dilsoor <abdudilsora@gmail.com>
2023-03-15 23:20:53 +0500

    portfoliodagi dardlarni qildim lekin nimasidir xato berdi

---
+++ src/protfoolio.js
@@ -0,0 +1,293 @@
+let section = document.querySelector(".showPanel");
+
+let tabObj1 = [
+  {
+    img: "images/portfolio/portfolio-1.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-2.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-3.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "SMM",
+  },
+];
+
+let tabObj2 = [
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-1.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-2.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-3.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "SMM",
+  },
+];
+
+let tabObj3 = [
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-1.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-2.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-3.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "SMM",
+  },
+];
+
+let tabObj4 = [
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-1.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-2.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-3.png",
+    title: "SMM",
+  },
+];
+
+let tabObj5 = [
+  {
+    img: "images/portfolio/portfolio-2.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-1.png",
+    title: "Mobil ilova",
+  },
+
+  {
+    img: "images/portfolio/portfolio-3.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "SMM",
+  },
+  {
+    img: "images/portfolio/portfolio-4.png",
+    title: "Mobil ilova",
+  },
+  {
+    img: "images/portfolio/portfolio-6.png",
+    title: "Veb sayt",
+  },
+  {
+    img: "images/portfolio/portfolio-5.png",
+    title: "SMM",
+  },
+];
+
+let arr=[tabObj1,tabObj2,tabObj3,tabObj4,tabObj5]
+
+function getCard(img, title) {
+  return `<div class="showPanel__card">
+    <img src="${img}" alt="">
+    <span>${title}</span>
+    <div class="showPanel__card-hover">
+      <img src="./images/portfolio/arrow.svg" alt="">
+      <p>UYMAKON mobil ilovasi</p>
+    </div>
+  </div>`;
+}
+
+function renderCardPrf(obj) {
+  for (const el of obj) {
+    section.innerHTML += getCard(el.img, el.title);
+  }
+}
+
+const tabBtn = document.querySelectorAll('.tabBtn li');
+const tabPanel = document.querySelectorAll('.tabPanel');
+tabBtn[0].style.background = "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
+
+function showTab(index) {
+    tabPanel.forEach(el=>{
+        el.style.display="none";
+    })
+    tabBtn.forEach(el=>{
+        el.style.background="transparent";
+    })
+    tabBtn[index].style.background="linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
+    renderCardPrf(arr[index]);
+}
+
+
