6a6ea23cc35228d2a1e8254fc24892dcc2eabae8
Dilsoor <abdudilsora@gmail.com>
2023-03-16 02:26:57 +0500

    home

--- src/protfoolio.js
+++ src/protfoolio.js
@@ -1,4 +1,5 @@
 let section = document.querySelector(".showPanel");
+let section2 = document.querySelector(".showPanel2");
 
 let tabObj1 = [
   {
@@ -256,7 +257,7 @@ let tabObj5 = [
   },
 ];
 
-let arr = [tabObj1, tabObj2, tabObj3, tabObj4, tabObj5]
+let arr = [tabObj1, tabObj2, tabObj3, tabObj4, tabObj5];
 
 function getCard(img, title) {
   return `<div class="showPanel__card">
@@ -275,18 +276,35 @@ function renderCardPrf(obj) {
   }
 }
 
-renderCardPrf(arr[0]);
+function renderCardHome(obj) {
+  for (let i = 0; i < 6; i++) {
+    section2.innerHTML += getCard(i.img, i.title);
+  }
+}
 
-const tabBtn = document.querySelectorAll('.tabBtn li');
-tabBtn[0].style.background = "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
+renderCardPrf(arr[0]);
+renderCardHome(arr[0]);
+
+const tabBtn = document.querySelectorAll(".tabBtn li");
+tabBtn[0].style.background =
+  "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
 
 function showTab(index) {
-  tabBtn.forEach(el => {
+  tabBtn.forEach((el) => {
     el.style.background = "transparent";
-  })
-  section.innerHTML="";
-  tabBtn[index].style.background = "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
+  });
+  section.innerHTML = "";
+  tabBtn[index].style.background =
+    "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
   renderCardPrf(arr[index]);
 }
 
-
+function showTabHome(index) {
+  tabBtn.forEach((el) => {
+    el.style.background = "transparent";
+  });
+  section2.innerHTML = "";
+  tabBtn[index].style.background =
+    "linear-gradient(93.5deg, #1E11AD 12.37%, rgba(105, 17, 173, 0.95) 85.28%)";
+  renderCardHome(arr[index]);
+}
